8dd17c4f703d8f98456adfb496232de012e2472b
Abrham Gebregiorgis <abrhamgg@gmail.com>
2023-01-12 14:10:09 +0300

    task-8 completed

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,4 @@
+-- script that creates an index idx_name_first on the table names and the first letter of name.
+-- Index is not the solution for any performance issue, but well used, it’s really powerful.
+
+CREATE INDEX idx_name_first ON names(name(1));
