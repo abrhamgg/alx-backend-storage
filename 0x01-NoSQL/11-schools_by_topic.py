321468ca059d026734680750f78403fb5d5749e6
abrhamgg <abrhamgg@gmail.com>
2023-01-17 01:50:14 +0300

    task-11 completed

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,6 +3,13 @@
 
 
 def schools_by_topic(mongo_collection, topic):
-    """function that returns the list of school having a specific topic"""
-    result = mongo_collection.find({"topic": topic})
-    return result
+    '''Returns the list of school having a specific topic.
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
