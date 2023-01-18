cbcfd19f9024120c2b2d6ea7b374472e7578f168
abrhamgg <abrhamgg@gmail.com>
2023-01-18 22:46:23 +0300

    completed

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,6 @@
  generate a random key (e.g. using uuid),
  store the input data in Redis using the random key and return the key.
 """
-
 from typing import Callable, Optional, Union
 from functools import wraps
 import redis
@@ -12,21 +11,25 @@ import sys
 from uuid import uuid4
 
 
+def count_calls(func: Callable) -> Callable:
+    """ a decorator to count how many times a function is called """
+    key: str = func.__qualname__
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        """ a function to be wrapped """
+        self._redis.incr(key, amount=1)
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
 class Cache:
-    "store an instance of redis client as private variable"
+    """store an instance of the Redis client as a private variable named"""
 
     def __init__(self) -> None:
+        """instantiate the class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        store the input data in Redis using the random key and return the key
-        """
-        key: str = str(uuid4())
-        value = data
-        self._redis.set(key, value)
-        return key
 
     def get_str(self, key: str) -> str:
         """ converts byte to str """
@@ -41,6 +44,15 @@ class Cache:
             data = 0
         return data
 
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        store the input data in Redis using the random key and return the key
+        """
+        key: str = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """ Gets  """
@@ -49,4 +61,4 @@ class Cache:
             res = fn(res) if fn else res
         except ValueError as err:
             pass
-        return
+        return res
