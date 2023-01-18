b7925ada23b1e8b725af3cdd4b7a4b1378475306
abrhamgg <abrhamgg@gmail.com>
2023-01-18 22:45:00 +0300

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,3 +27,26 @@ class Cache:
         value = data
         self._redis.set(key, value)
         return key
+
+    def get_str(self, key: str) -> str:
+        """ converts byte to str """
+        data = self._redis.get(key)
+        return data.decode('UTF-8')
+
+    def get_int(self, key: str) -> int:
+        data = self._redis.get(key)
+        try:
+            data = int(data.decode('UTF-8'))
+        except Exception:
+            data = 0
+        return data
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Gets  """
+        res = self._redis.get(key)
+        try:
+            res = fn(res) if fn else res
+        except ValueError as err:
+            pass
+        return
